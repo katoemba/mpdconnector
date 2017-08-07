7501e06d6a0ecf53c3d6421b793eef59a0975b3a
Berrie Kremers <berrie.kremers@xs4all.nl>
2017-08-07 21:28:55 +0200

    initial commit of MPDConnector

---
+++ MPDConnector_iOS/MPDConnector_iOS.h
@@ -0,0 +1,19 @@
+//
+//  MPDConnector_iOS.h
+//  MPDConnector
+//
+//  Created by Berrie Kremers on 05-08-17.
+//  Copyright © 2017 Kaotemba Software. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MPDConnector.
+FOUNDATION_EXPORT double MPDConnectorVersionNumber;
+
+//! Project version string for MPDConnector.
+FOUNDATION_EXPORT const unsigned char MPDConnectorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MPDConnector/PublicHeader.h>
+
+
