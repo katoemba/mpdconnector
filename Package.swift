7d0be831ebe967ff69fc5b1cd2a6d2f160339d84
Berrie Kremers <berrie.kremers@icloud.com>
2025-12-11 21:15:15 +0100

    Use toolchain 5.10.

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "MPDConnector",
     defaultLocalization: "en",
-    platforms: [.macOS(.v15), .iOS(.v18), .watchOS(.v11), .tvOS(.v18)],
+    platforms: [.macOS(.v14), .iOS(.v17), .watchOS(.v10), .tvOS(.v17)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(name: "MPDConnector", targets: ["MPDConnector"]),
