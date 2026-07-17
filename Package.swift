e834e483ad0dfb71f9f26afe7a40bb29f83d4d72
Berrie Kremers <berrie.kremers@icloud.com>
2026-07-17 19:33:50 +0200

    Require at least iOS 15.

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "MPDConnector",
-    platforms: [.macOS(.v12), .iOS(.v14), .watchOS(.v10)],
+    platforms: [.macOS(.v12), .iOS(.v15), .watchOS(.v10)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(name: "MPDConnector", targets: ["MPDConnector"]),
